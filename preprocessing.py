6c05195ebd8ac5d32b21bb5ecb500fcb748a322c
Eszes Bálint <eszes.balint96@gmail.com>
2018-03-18 18:42:55 +0100

    commandline input options added

--- preprocessing.py
+++ preprocessing.py
@@ -2,8 +2,9 @@ import numpy as np
 import nbt
 import sys
 
-window_size=(5,5,5)
+window_size=(10,10,10)
 offset = (int(window_size[0]/2),int(window_size[1]/2),int(window_size[2]/2))
+tile_size=(10,10,10)
 
 file = nbt.load(sys.argv[1])
 
@@ -16,15 +17,20 @@ data = data.reshape(y,z,x)
 
 c=0
 for i in range(y-offset[0]*2):
+	if(i%tile_size[0]!=0): continue
 	for j in range(z-offset[1]*2):
+		if(j%tile_size[1]!=0): continue
 		for k in range(x-offset[2]*2):
+			if(k%tile_size[2]!=0): continue
 			feature = data[
 				i:i+2*offset[0]+1,
 				j:j+2*offset[1]+1,
 				k:k+2*offset[2]+1,
 				]
-			if(feature.sum()):
-				c+=1
+			if(not feature.sum()):
+				continue
+			c+=1
+			np.save("{}".format(c),feature)
 
 print(c)
 
--- voxel_viz.py
+++ voxel_viz.py
@@ -6,37 +6,46 @@ import nbt
 import sys
 
 
-file = nbt.load(sys.argv[1])
+if(sys.argv[1].endswith('.schematic')):
+	file = nbt.load(sys.argv[1])
+	
+	x = file.__getitem__(u'Width').value
+	y = file.__getitem__(u'Height').value
+	z = file.__getitem__(u'Length').value
+	
+	data = file.__getitem__(u'Blocks').value
+	data = data.reshape(y,z,x)
+elif(sys.argv[1].endswith('.npy')):
+	data = np.load(sys.argv[1])
 
-x = file.__getitem__(u'Width').value
-y = file.__getitem__(u'Height').value
-z = file.__getitem__(u'Length').value
-
-data = file.__getitem__(u'Blocks').value
-data = data.reshape(y,z,x)
-
-
-
-
-
-
-
-#fig = plt.figure()
-#ax = fig.gca(projection='3d')
-#ax.voxels(data,edgecolor='k')
-
-
-
-
-plt.subplot(131)
-plt.imshow(np.average(data,0), cmap='hot', interpolation='nearest', origin='lower')
+if(sys.argv[2]=="3d"):
+	fig = plt.figure()
+	ax = fig.gca(projection='3d')
+	ax.voxels(data,edgecolor='k')
+	plt.show()
+elif(sys.argv[2]=="2d"):
+	
+	plt.subplot(221)
+	plt.imshow(np.count_nonzero(data,0), cmap='Spectral', interpolation='nearest', origin='lower')
 
 
-plt.subplot(132)
-plt.imshow(np.average(data,1), cmap='hot', interpolation='nearest', origin='lower')
+	plt.subplot(222)
+	plt.imshow(np.count_nonzero(data,1), cmap='Spectral', interpolation='nearest', origin='lower')
 
 
-plt.subplot(133)
-plt.imshow(np.average(data,2), cmap='hot', interpolation='nearest', origin='lower')
-plt.show()
+	plt.subplot(223)
+	plt.imshow(np.count_nonzero(data,2), cmap='Spectral', interpolation='nearest', origin='lower')
+	plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
 

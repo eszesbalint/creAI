9a3807c80d8f1757492e591d4e2920883de1daee
Eszes Bálint <eszes.balint96@gmail.com>
2020-04-03 13:08:03 +0200

    Updated dependencies

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(name='creAI', version='0.1', packages=find_packages())
+setup(
+    name='creAI',
+    version='0.1',
+    author='Eszes Bálint',
+    author_email='eszes.balint96@gmail.com',
+    url='https://github.com/ebalint96/creAI',
+    packages=find_packages(),
+    install_requires=[
+        'Eel',
+        'Pillow',
+        'numpy'
+    ]
+    )

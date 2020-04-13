bba65b2f063f429662c83ba98f0c808403b976a0
Eszes Bálint <eszes.balint96@gmail.com>
2020-04-13 20:12:56 +0200

    Fixing import errors

--- creAI/plugins/__init__.py
+++ creAI/plugins/__init__.py
@@ -1,10 +1,9 @@
-import os
-
-modules = [name for name in os.listdir(os.path.dirname(__file__)) if os.path.isdir(
-    os.path.join(os.path.dirname(__file__), name)) and name != '__pycache__']
-
-for module in modules:
-    __import__('creAI.plugins.' + module, locals(), globals())
-
-del modules
-
+#import os
+#
+# modules = [name for name in os.listdir(os.path.dirname(__file__)) if os.path.isdir(
+#    os.path.join(os.path.dirname(__file__), name)) and name != '__pycache__']
+#
+# for module in modules:
+#    __import__('creAI.plugins.' + module, locals(), globals())
+#
+#del modules
